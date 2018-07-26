adfbba36fe2bf2b50d5e44fe8ebbc14af7ec9127
Felipe Sere <felipesere@gmail.com>
2018-07-26 22:14:21 +0100

    Read a path from dynamodb

--- lessons_handler.js
+++ lessons_handler.js
@@ -1,15 +1,57 @@
 'use strict'
 
+const AWS = require('aws-sdk')
+const dynamoDb = new AWS.DynamoDB();
+
+const APPRENTICES_TABLE = process.env.DYNAMODB_TABLE;
+
 const {lessons} = require('./lessons')
 
-module.exports.all_lessons = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(lessons),
-    headers: {
-      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+const all_lessons = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(lessons),
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
     }
-  }
 
-  callback(null, response)
+    callback(null, response)
+}
+
+const where = ({email}) => {
+    return {
+        Key: {
+            "email": {
+                S: email
+            },
+        },
+        TableName: APPRENTICES_TABLE
+    };
+}
+
+const read_path = (event, context, callback) => {
+    const apprentice = event.pathParameters.apprentices_id
+    console.log({apprentice})
+    console.log("TABLE: ", APPRENTICES_TABLE)
+    const params = {
+        Key: {
+            "email": {
+                S: apprentice
+            },
+        },
+        TableName: APPRENTICES_TABLE
+    };
+    dynamoDb.getItem(params, (err, data) => {
+        if (err) {
+            callback(null, {statusCode: 404, body: `Did not find ${apprentice}: ${JSON.stringify(err)} `})
+        } else {
+            callback(null, {statusCode: 200, body: JSON.stringify(data)})
+        }
+    })
+}
+
+module.exports = {
+    all_lessons: all_lessons,
+    read_path: read_path
 }
