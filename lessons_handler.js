e5626df75fabe732d8e0194a2e1fecbf8db94303
Felipe Sere <felipesere@gmail.com>
2018-07-26 22:55:30 +0100

    Add prettier

--- lessons_handler.js
+++ lessons_handler.js
@@ -1,45 +1,56 @@
-'use strict'
+"use strict";
 import AWS from "aws-sdk";
-import {lessons} from "./lessons";
+import { lessons } from "./lessons";
 
 const dynamoDb = new AWS.DynamoDB();
 const APPRENTICES_TABLE = process.env.DYNAMODB_TABLE;
 
 const all_lessons = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(lessons),
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(lessons),
+    headers: {
+      "Access-Control-Allow-Origin": "*"
     }
+  };
 
-    callback(null, response)
-}
+  callback(null, response);
+};
 
-const where = ({email}) => {
-    return {
-        Key: {
-            "email": {
-                S: email
-            },
-        },
-        TableName: APPRENTICES_TABLE
-    };
-}
+const where = ({ email }) => {
+  return {
+    Key: {
+      email: {
+        S: email
+      }
+    },
+    TableName: APPRENTICES_TABLE
+  };
+};
+
+const notFound = (apprentice, err) => {
+  return {
+    statusCode: 404,
+    body: `Did not find ${apprentice}: ${JSON.stringify(err)} `
+  };
+};
+
+const found = data => {
+  return { statusCode: 200, body: JSON.stringify(data) };
+};
 
 const read_path = (event, context, callback) => {
-    const apprentice = event.pathParameters.apprentices_id
-    dynamoDb.getItem(where({email: apprentice}), (err, data) => {
-        if (err) {
-            callback(null, {statusCode: 404, body: `Did not find ${apprentice}: ${JSON.stringify(err)} `})
-        } else {
-            callback(null, {statusCode: 200, body: JSON.stringify(data)})
-        }
-    })
-}
+  const apprentice = event.pathParameters.apprentices_id;
+  dynamoDb.getItem(where({ email: apprentice }), (err, data) => {
+    if (err) {
+      callback(null, notFound(apprentice, err));
+    } else {
+      callback(null, found(data));
+    }
+  });
+};
 
 module.exports = {
-    all_lessons,
-    read_path
-}
+  all_lessons,
+  read_path
+};
